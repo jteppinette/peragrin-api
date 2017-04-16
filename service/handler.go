7a02e768c7f7162b839ba46cf4528b63b868598f
Joshua Taylor Eppinette <josh.eppinette@peragrin.com>
2017-04-16 14:46:14 -0400

    Add service Response and Handler types

---
+++ service/handler.go
@@ -0,0 +1,55 @@
+package service
+
+import (
+	"net/http"
+
+	"github.com/unrolled/render"
+)
+
+var (
+	rend = render.New().JSON
+)
+
+// Response represents the return value to the Handler type.
+// This struct encapsulates the information need to log and write
+// at the end of a request/response cycle.
+type Response struct {
+	Error error
+	Code  int
+	Data  interface{}
+}
+
+// NewResponse returns an initialized response pointer.
+func NewResponse(err error, code int, data interface{}) *Response {
+	return &Response{err, code, data}
+}
+
+// Handler overrides the typical http.Handler interface with the Response
+// return value. This allows the types ServeHTTP function to handle the Response
+// in a single place providing standardized logging and response writing.
+type Handler func(w http.ResponseWriter, r *http.Request) *Response
+
+// ServeHTTP calls the handler's underlying function, and it will properly
+// handle the returned response.
+// If the response or response data is nil, an empty text/html response will
+// be returned. Otherwise, the response data will be written as encoded JSON.
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	response := h(w, r)
+
+	// If the response is nil, then use http.StatusOK as the default HTTP code.
+	var code int
+	if response == nil {
+		code = http.StatusOK
+	} else {
+		code = response.Code
+	}
+
+	// If the response or response data is nil, then write the calculated code
+	// as a default empty text/html response.
+	if response == nil || response.Data == nil {
+		w.WriteHeader(code)
+		return
+	}
+
+	rend(w, code, response.Data)
+}
---
+++ service/handler_test.go
@@ -0,0 +1,52 @@
+package service
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	tests := []struct {
+		handler Handler
+		code    int
+		bytes   []byte
+	}{
+		{
+			func(w http.ResponseWriter, r *http.Request) *Response {
+				data := struct {
+					Detail string `json:"detail"`
+				}{"hey"}
+				return NewResponse(nil, http.StatusOK, data)
+			},
+			http.StatusOK,
+			[]byte(`{"detail":"hey"}`),
+		},
+		{
+			func(w http.ResponseWriter, r *http.Request) *Response {
+				return NewResponse(nil, http.StatusOK, nil)
+			},
+			http.StatusOK,
+			nil,
+		},
+		{
+			func(w http.ResponseWriter, r *http.Request) *Response {
+				return nil
+			},
+			http.StatusOK,
+			nil,
+		},
+	}
+	for _, test := range tests {
+		w := httptest.NewRecorder()
+		test.handler.ServeHTTP(w, &http.Request{})
+		if w.Code != test.code {
+			t.Errorf("expected code to be %d, got %d", test.code, w.Code)
+		}
+		b := w.Body.Bytes()
+		if !bytes.Equal(b, test.bytes) {
+			t.Errorf("expected response to be %s, got %s", test.bytes, b)
+		}
+	}
+}

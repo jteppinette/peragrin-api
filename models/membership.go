cfd512fe32b1aa404123917e003c8e0f2b0e81e8
Joshua Taylor Eppinette <josh.eppinette@peragrin.com>
2017-08-21 20:14:15 -0400

    Add price to membership

--- models/membership.go
+++ models/membership.go
@@ -10,10 +10,11 @@ import (
 // Membership represents a level of membership that
 // a patron could possess with a community.
 type Membership struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	CommunityID int    `json:"communityID,omitempty"`
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	CommunityID int     `json:"communityID,omitempty"`
+	Price       float64 `json:"price"`
 
 	// Expiration is used to define the time left for a provided account membership.
 	// This information is only useful when in the context of an account.
@@ -22,18 +23,18 @@ type Membership struct {
 
 // Create adds a new membership to the database.
 func (m *Membership) Create(communityID int, client *sqlx.DB) error {
-	return client.Get(m, "INSERT INTO Membership (communityID, name, description) VALUES ($1, $2, $3) RETURNING id, name, description;", communityID, m.Name, m.Description)
+	return client.Get(m, "INSERT INTO Membership (communityID, name, description, price) VALUES ($1, $2, $3, $4) RETURNING id, name, description, price;", communityID, m.Name, m.Description, m.Price)
 }
 
 // Update updates a membership row in the database.
 func (m *Membership) Update(client *sqlx.DB) error {
-	return client.Get(m, "UPDATE Membership SET name = $2, description = $3 WHERE id = $1 RETURNING id, name, description;", m.ID, m.Name, m.Description)
+	return client.Get(m, "UPDATE Membership SET name = $2, description = $3, price = $4 WHERE id = $1 RETURNING id, name, description, price;", m.ID, m.Name, m.Description, m.Price)
 }
 
 // GetMembershipsByCommunity returns all of a communities' memberships.
 func GetMembershipsByCommunity(communityID int, client *sqlx.DB) ([]Membership, error) {
 	memberships := []Membership{}
-	if err := client.Select(&memberships, "SELECT id, name, description FROM Membership WHERE communityID = $1;", communityID); err != nil {
+	if err := client.Select(&memberships, "SELECT id, name, description, price FROM Membership WHERE communityID = $1 ORDER BY price;", communityID); err != nil {
 		return nil, err
 	}
 	return memberships, nil
@@ -42,7 +43,7 @@ func GetMembershipsByCommunity(communityID int, client *sqlx.DB) ([]Membership, 
 // GetMembershipByID returns the requested membership.
 func GetMembershipByID(membershipID int, client *sqlx.DB) (*Membership, error) {
 	m := &Membership{}
-	if err := client.Get(m, "SELECT id, name, description FROM Membership WHERE id = $1;", membershipID); err == sql.ErrNoRows {
+	if err := client.Get(m, "SELECT id, name, description, price FROM Membership WHERE id = $1;", membershipID); err == sql.ErrNoRows {
 		return nil, nil
 	} else if err != nil {
 		return nil, err
@@ -54,9 +55,9 @@ func GetMembershipByID(membershipID int, client *sqlx.DB) (*Membership, error) {
 func GetMembershipsByAccount(accountID int, client *sqlx.DB) ([]Membership, error) {
 	memberships := []Membership{}
 	if err := client.Select(&memberships, `
-		SELECT Membership.id, Membership.name, Membership.description, Membership.communityID, AccountMembership.expiration FROM Membership
+		SELECT Membership.id, Membership.name, Membership.description, Membership.price, Membership.communityID, AccountMembership.expiration FROM Membership
 		INNER JOIN AccountMembership ON (Membership.id = AccountMembership.membershipID)
-		WHERE AccountMembership.accountID = $1;
+		WHERE AccountMembership.accountID = $1 ORDER BY Membership.price;
 	`, accountID); err != nil {
 		return nil, err
 	}
